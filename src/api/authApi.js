3543c237e4b0daab1fc567caf193663374afc61a
Ansh Talaviya <anshtalaviya22@gmail.com>
2025-05-21 15:19:29 +0530

    init

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import api from "./index"
 
 export const authApi = {
-  register: (userData) => axios.post("http://localhost:8899/api/register", userData),
+  register: (userData) => axios.post("/api/register", userData),
   login: (credentials) => api.post("/api/login", credentials),
   resendOtp: (email) => api.post("/api/resend-otp", { email }),
 }
